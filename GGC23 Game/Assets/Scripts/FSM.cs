5715b7c19edc7ec407dd04f2f6438e7d057bbfa8
Afo262414 <idv20c.aflores@uartesdigitales.edu.mx>
2023-02-01 18:38:54 -0600

    Commit StateMachine
    Add, Movement, Run and Idle State

---
+++ GGC23 Game/Assets/Scripts/FSM.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    //update llama al onupdate del estado actual
+    // Start de la maquina para inicializar los estados
+
+    public State m_current;
+    public State m_Walk;
+    public State m_Run;
+    public State m_Idle;
+    public Rigidbody2D Rigid;
+    public Animator animator;
+    public void SetState(State newState)
+    {
+        m_current.OnExit();
+        m_current = newState;
+        m_current.OnEnter();
+    }
+    void Start()
+    {
+        m_Walk = gameObject.AddComponent<Walk>();
+        m_Run = gameObject.AddComponent<Run>();
+        m_Idle = gameObject.AddComponent<Idle>();
+        m_Walk.SetFSM(this);
+        m_Run.SetFSM(this);
+        m_Idle.SetFSM(this);
+        m_current = m_Idle;
+        m_current.OnEnter();
+        Rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        m_current.OnUpdate();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        m_current.OnCollisionE(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        m_current.OntriggerS(collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        m_current.OntriggerEx(collision);
+    }
+}
---
+++ GGC23 Game/Assets/Scripts/State.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    public Animator animator;
+    public FSM m_statemachine;
+    virtual public void OnEnter(){
+
+    }
+    virtual public void OnExit(){
+
+    }
+    virtual public void OnUpdate(){
+
+    }
+    virtual public void OnCollisionE(Collision2D collision)
+    {
+        
+    }
+    virtual public void OntriggerS(Collider2D collision)
+    {
+
+    }
+    virtual public void OntriggerEx(Collider2D collision)
+    {
+
+    }
+    public void SetFSM(FSM SM){
+        m_statemachine = SM;
+    }
+    private void Awake()
+    {
+        animator = GameObject.Find("Player").GetComponent<Animator>();
+    }
+}
